6b20b476afe075c5c1ad7f2ada8025d5b2c5e397
Luca Lambiase <luca.lambia@gmail.com>
2023-11-02 11:57:16 +0100

    feat: add includes example and domain filter

--- js/mail.js
+++ js/mail.js
@@ -2,26 +2,41 @@ const listaUtenti = ["luca@mail.it", "fra@mail.it", "marco@mail.it", "lore@mail.
 
 const mailUtente = prompt("Inserisci la tua mail");
 
-// console.log(mailUtente);
+let autorizzato = false;
 
-let mailTrovata = false;
+// alternativa al ciclo for
+// autorizzato = listaUtenti.includes(mailUtente);
 
 for (let i = 0; i < listaUtenti.length; i++) {
     
     const element = listaUtenti[i];
 
-    console.log( element, i );
+    // console.log( element, i );
     
     if(element == mailUtente) {
-        mailTrovata = true;
+        autorizzato = true;
         break;
     }
     
-    // console.log(mailTrovata, i);
+    // console.log(autorizzato, i);
 }
 
-if(mailTrovata) {
-    alert("Benvenuto, " + mailUtente);
+let nomeUtente = "";
+for (let i = 0; i < mailUtente.length; i++) {
+    const carattere = mailUtente[i];
+
+    if(carattere=="@") {
+        break;
+    }
+    nomeUtente += carattere;
+}
+
+//Alternativa con indexOf e substring
+// const posizioneChiocciola = mailUtente.indexOf("@");
+// nomeUtente = mailUtente.substring(0, posizioneChiocciola);
+
+if(autorizzato) {
+    alert("Benvenuto, " + nomeUtente);
 } else {
     alert("Non sei autorizzato, vai via!");
 }
